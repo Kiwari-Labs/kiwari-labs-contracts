e950ab577f2c6cf05df2a101d1dcd40d04c05d96
MASDXI <sirawitt42@gmail.com>
2024-04-02 13:12:47 +0700

    update triedb note

--- contracts/TrieDB.sol
+++ contracts/TrieDB.sol
@@ -2,14 +2,30 @@
 pragma solidity 0.8.14;
 
 contract TrieDB {
+    // @todo specification
+    // - MUST be abstract contract/class for easy implement with existing tools.
+    // - MUST be extenable feature freeze, expiration, or memotag for flexibilities.
+    // - MUST be support backward compatibilities with ERC20 standard interface.
+    // - SHOULD be execute with reasonable/affordable gas used.
 
-    // @TODO 
-    // - MUST be abstract contract/class
-    // - MUST extenable feature freeze, expiration, memotag
-    // - SHOULD support ERC20 interface
-    // - SHOULD execute with reasonable gas used
+    // as-in can done with existing code
+    // for research paper need to adding blacklist feature, freeze suspicious
+    // to-be
+    // specify freeze money lenght
+    // a
+    // |-----|
+    // |     |
+    // a1   a2
+    //      |-----|
+    //      |    a3 (freeze)
+    //      |
+    //      |----------------------------|
+    //      a2 (non freeze)             a4 (non freeze)
 
-    enum TRIE_STATUS { INACTIVE, ACTIVE }
+    enum TRIE_STATUS {
+        INACTIVE,
+        ACTIVE
+    }
 
     struct TrieNode {
         bytes32 origin;
@@ -29,7 +45,12 @@ contract TrieDB {
         return bytes32(abi.encode(keccak256(abi.encode(account, Id))));
     }
 
-    function _modifyTrie(address account, uint256 amount, bytes32 origin, bytes32 data) internal {
+    function _modifyTrie(
+        address account,
+        uint256 amount,
+        bytes32 origin,
+        bytes32 data
+    ) internal {
         // require validation input
         bytes32 TrieHash = _hash(account, _trieCount[account]);
         TrieNode storage trie = _trie[TrieHash];
@@ -40,12 +61,22 @@ contract TrieDB {
             _trieCount[account]++;
             _createOrUpdateTransaction(trie, data, amount);
         } else {
-            Transaction storage lastTransaction = trie.txChange[trie.txChange.length - 1];
-            _createOrUpdateTransaction(trie, data, lastTransaction.amount - amount);
+            Transaction storage lastTransaction = trie.txChange[
+                trie.txChange.length - 1
+            ];
+            _createOrUpdateTransaction(
+                trie,
+                data,
+                lastTransaction.amount - amount
+            );
         }
     }
 
-    function _createOrUpdateTransaction(TrieNode storage trie, bytes32 data, uint256 newAmount) internal {
+    function _createOrUpdateTransaction(
+        TrieNode storage trie,
+        bytes32 data,
+        uint256 newAmount
+    ) internal {
         if (newAmount != 0) {
             trie.txChange.push(Transaction(newAmount, data));
         } else {
@@ -54,7 +85,12 @@ contract TrieDB {
         }
     }
 
-    function _create(address account, uint256 amount, bytes32 origin, bytes32 data) public {
+    function _create(
+        address account,
+        uint256 amount,
+        bytes32 origin,
+        bytes32 data
+    ) public {
         _modifyTrie(account, amount, origin, data);
     }
 
@@ -63,27 +99,45 @@ contract TrieDB {
         TrieNode storage currentTrie = _trie[trieHash];
 
         // Ensure the current trie is in an active state and has enough balance
-        require(currentTrie.status == TRIE_STATUS.ACTIVE, "Current trie is not active");
-        require(currentTrie.txChange.length > 0 && currentTrie.txChange[currentTrie.txChange.length - 1].amount >= amount, "Insufficient balance");
+        require(
+            currentTrie.status == TRIE_STATUS.ACTIVE,
+            "Current trie is not active"
+        );
+        require(
+            currentTrie.txChange.length > 0 &&
+                currentTrie.txChange[currentTrie.txChange.length - 1].amount >=
+                amount,
+            "Insufficient balance"
+        );
 
         // Calculate the remaining amount needed
         uint256 remainingAmount = amount;
 
         // Loop through tries until the remaining amount is satisfied
         // @TODO research implementing to loop through only active trie
-        for (uint256 i = 0; i < _trieCount[msg.sender] && remainingAmount > 0; i++) {
+        for (
+            uint256 i = 0;
+            i < _trieCount[msg.sender] && remainingAmount > 0;
+            i++
+        ) {
             // Load struct trie from storage
             TrieNode storage trie = _trie[_hash(msg.sender, i)];
 
             // Ensure the trie is in an active state before proceeding
             require(trie.status == TRIE_STATUS.ACTIVE, "Trie is not active");
 
-            Transaction memory lastTransaction = trie.txChange.length > 0 ? trie.txChange[trie.txChange.length - 1] : Transaction(0, "");
+            Transaction memory lastTransaction = trie.txChange.length > 0
+                ? trie.txChange[trie.txChange.length - 1]
+                : Transaction(0, "");
             // Check if the trie has enough balance
             if (lastTransaction.amount >= remainingAmount) {
                 // If yes, modify the trie and exit the loop
                 _modifyTrie(account, remainingAmount, trie.origin, "");
-                _createOrUpdateTransaction(trie, "", lastTransaction.amount - remainingAmount);
+                _createOrUpdateTransaction(
+                    trie,
+                    "",
+                    lastTransaction.amount - remainingAmount
+                );
                 remainingAmount = 0;
             } else {
                 // If no, deduct the remaining balance from the trie and continue to the next trie
@@ -93,17 +147,30 @@ contract TrieDB {
             }
         }
     }
-    
-    function _spend(address account, uint256 amount, uint256 id, bytes32 data) public {
+
+    function _spend(
+        address account,
+        uint256 amount,
+        uint256 id,
+        bytes32 data
+    ) public {
         bytes32 TrieHashOrigin = _hash(msg.sender, id);
         TrieNode storage originTrie = _trie[TrieHashOrigin];
-        Transaction storage lastTransaction = originTrie.txChange[originTrie.txChange.length - 1];
+        Transaction storage lastTransaction = originTrie.txChange[
+            originTrie.txChange.length - 1
+        ];
 
         _modifyTrie(account, amount, originTrie.origin, "");
-        _createOrUpdateTransaction(_trie[_hash(msg.sender, id)], data, lastTransaction.amount - amount);
+        _createOrUpdateTransaction(
+            _trie[_hash(msg.sender, id)],
+            data,
+            lastTransaction.amount - amount
+        );
     }
 
-    function getTrie(bytes32 TrieId) public view returns (TrieNode memory, Transaction memory) {
+    function getTrie(
+        bytes32 TrieId
+    ) public view returns (TrieNode memory, Transaction memory) {
         TrieNode storage trieCache = _trie[TrieId];
         return (trieCache, trieCache.txChange[trieCache.txChange.length - 1]);
     }
