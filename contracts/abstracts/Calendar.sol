24275a60dcee96d2c0efe7ae717219d852b72067
MASDXI <sirawitt42@gmai.com>
2024-04-08 14:31:29 +0700

    tighten variable size

--- contracts/abstracts/Calendar.sol
+++ contracts/abstracts/Calendar.sol
@@ -7,20 +7,21 @@ import "../interfaces/ICalendar.sol";
 
 abstract contract Calendar is ICanlendar {
 
-    // 18 bytes for constant variables
+    // 49 bytes allocated for global variables
     uint8 private constant SLOT_PER_ERA = 8;
     uint8 private constant MINIMUM_EXPIRE_PERIOD_SLOT = 1;
-    uint16 private constant MAXIMUM_EXPIRE_PERIOD_SLOT = 16;
-    uint16 private constant MINIMUM_BLOCKTIME_IN_MILLI_SECONDS = 1;        // 1 milliseconds.
-    uint32 private constant MAXIMUM_BLOCKTIME_IN_MILLI_SECONDS = 600_000 ; // 10 minutes.
-    uint64 private constant YEAR_IN_MILLI_SECONDS = 31_556_926_000;
+    uint8 private constant MAXIMUM_EXPIRE_PERIOD_SLOT = 16;
+    uint8 private constant MINIMUM_BLOCKTIME_IN_MILLI_SECONDS = 1;         // 1 milliseconds.
+
+    uint16 private constant MAXIMUM_BLOCKTIME_IN_MILLI_SECONDS = 600_000 ; // 10 minutes.
+    uint40 private constant YEAR_IN_MILLI_SECONDS = 31_556_926_000;
+
+    uint8 private _expirePeriod;
+    uint40 private _blockPerYear;  // uint40 is enough cause it can fit wrost case 1 ms 31,556,926,000 block per year.
 
     uint256 private immutable _startBlockNumber;
 
-    uint8 private _expirePeriod;
-    uint256 private _blockPerYear;  // uint64 is enough cause it can fit wrost case 1 ms 31,556,926,000 block per year.
-
-    constructor (uint32 blockTime_, uint256 blockNumber_, uint16 expirePeriod_) {
+    constructor (uint16 blockTime_, uint256 blockNumber_, uint8 expirePeriod_) {
         _startBlockNumber = blockNumber_;
         _updateBlockPerYear(blockTime_);
         _updateExpirePeriod(expirePeriod_);
@@ -33,7 +34,7 @@ abstract contract Calendar is ICanlendar {
         //         blockTime > MAXIMUM_BLOCKTIME_IN_MILLI_SECONDS) {
         //     revert InvalidBlockPeriod();
         // }
-        uint256 blockPerYearCache = _blockPerYear;
+        uint40 blockPerYearCache = _blockPerYear;
         _blockPerYear = YEAR_IN_MILLI_SECONDS / blockTime;
         // @TODO uncomment
         // emit BlockProducedPerYearUpdated(blockPerYearCache, blockTime);
@@ -90,8 +91,8 @@ abstract contract Calendar is ICanlendar {
         return (era, slot);
     }
 
-    function _calculateBlockDifferent(uint256 blockNumber) internal view returns (uint256) {
-        uint256 expirePeriodInBlockLength = expirationPeriodInBlockLength();
+    function _calculateBlockDifferent(uint256 blockNumber) internal view returns (uint64) {
+        uint64 expirePeriodInBlockLength = expirationPeriodInBlockLength();
         if (blockNumber >= expirePeriodInBlockLength) {
             // If the current block is beyond the expiration period
             return blockNumber - expirePeriodInBlockLength;
@@ -101,7 +102,7 @@ abstract contract Calendar is ICanlendar {
         }
     }
 
-    function _addingBuffer(uint256 startBlock) internal view returns (uint256 fromEra, uint8 fromSlot) {
+    function _calulateFromEraAndFromSlot(uint256 startBlock) internal view returns (uint256 fromEra, uint8 fromSlot) {
         // Determine the starting era and slot based on the expiration start block
         if (startBlock >= _blockPerYear) {
             (fromEra, fromSlot) = _calculateEraAndSlot(startBlock);
@@ -109,39 +110,47 @@ abstract contract Calendar is ICanlendar {
             fromEra = 0;
             fromSlot = uint8(startBlock % _blockPerYear);
         }
+        (fromEra, fromSlot) = _addingBuffer(fromEra, fromSlot);
+        return (fromEra, fromSlot);
+    }
 
+    function _addingBuffer(uint256 era, uint8 slot) private view returns (uint256 , uint8) {
         // Add buffer slot
-        if (fromEra != 0 && fromSlot != 0) {
-            if (fromSlot > 0) {
-                fromSlot--;
+        if (era != 0 && slot != 0) {
+            if (slot > 0) {
+                slot--;
             } else {
-                fromEra--;
-                fromSlot = 7;
+                era--;
+                slot = 7;
             }
         }
-        return (fromEra, fromSlot);
+        return (era, slot);
     }
 
     function _safePagination() public view returns (uint256 fromEra, uint256 toEra, uint8 fromSlot, uint8 toSlot) {
         uint256 blockNumberCache = blockNumberProvider();
         (toEra, toSlot) = _calculateEraAndSlot(blockNumberCache);
         // Calculate the starting block for the expiration period
-        uint256 expirationStartBlock = _calculateBlockDifferent(blockNumberCache);
+        uint256 fromBlock = _calculateBlockDifferent(blockNumberCache);
         // Buffering for ensure
-        (fromEra, fromSlot) = _addingBuffer(expirationStartBlock);
+        (fromEra, fromSlot) = _calulateFromEraAndFromSlot(fromBlock);
         return (fromEra, toEra, fromSlot, toSlot);
     }
 
-    function currentEra() public view override returns (uint256) {
-        return _calculateEra(blockNumberProvider());
+    function _blockNumberProvider() internal view virtual override returns (uint256) {
+        return block.number;
     }
 
-    function currentSlot() public view override returns (uint16) {
-        return _calculateSlot(blockNumberProvider());
+    function currentEra() public view override returns (uint256) {
+        return _calculateEra(_blockNumberProvider());
+    }
+
+    function currentSlot() public view override returns (uint8) {
+        return _calculateSlot(_blockNumberProvider());
     }
 
     function currentEraAndSlot() public view override returns (uint256 era, uint8 slot) {
-        (era, slot) = _calculateEraAndSlot(blockNumberProvider());
+        (era, slot) = _calculateEraAndSlot(_blockNumberProvider());
         return (era, slot);
     }
 
@@ -150,18 +159,41 @@ abstract contract Calendar is ICanlendar {
         return (fromEra, toEra, fromSlot, toSlot);
     }
 
-    function blockNumberProvider() public view virtual override returns (uint256) {
-        return block.number;
-    }
-
-    /// @return uint256 amount of blocks per era.
-    function blockPerEra() public view override returns (uint256) {
+    /// @return uint40 amount of blocks per era.
+    function blockPerEra() public view override returns (uint40) {
         return _blockPerYear;
     }
 
-    /// @return uint256 amount of blocks per slot.
-    function blockPerSlot() public view override returns (uint256) {
+    /// @return uint40 amount of blocks per slot.
+    function blockPerSlot() public view override returns (uint40) {
         return _blockPerYear / SLOT_PER_ERA;
+    }
+
+    function slotPerEra() pulbic view override returns (uint8) {
+        return SLOT_PER_ERA;
+    }
+
+     /// @return uint40 length of blocks.
+    function expirationPeriodInBlockLength() public view override returns (uint40) {
+        return blockPerSlot() * _expirePeriod;
+    }
+
+    /// @return uint8 length of slot.
+    function expirationPeriodInSlotLegth() public view override returns (uint8) {
+        return _expirePeriod;
+    }
+
+    /// @return era cycle.
+    /// @return slot of slot.
+    function expirationPeriodInEraLength() public view override returns (uint8 era, uint8 slot) {
+        if (_expirePeriod <= SLOT_PER_ERA) {
+            era = 0;
+            slot = uint8(_expirePeriod);
+        } else {
+            era = uint8(_expirePeriod / SLOT_PER_ERA);
+            slot = uint8(_expirePeriod % SLOT_PER_ERA);
+        }
+        return (era, slot);
     }
 
 } 
