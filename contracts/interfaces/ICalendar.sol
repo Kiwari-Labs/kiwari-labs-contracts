6a2de77d6a2b74934a5e4f7e1a237d0467eed65c
MASDXI <sirawitt42@gmai.com>
2024-04-09 10:04:33 +0700

    change solidity to UNLICENSED

--- contracts/interfaces/ICalendar.sol
+++ contracts/interfaces/ICalendar.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: MIT
+// SPDX-License-Identifier: UNLICENSED
 pragma solidity >=0.5.0 <0.8.0;
 
 /// @author ERC20EXP <erc20exp@protonmail.com>
--- contracts/interfaces/IERC20EXP.sol
+++ contracts/interfaces/IERC20EXP.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: MIT
+// SPDX-License-Identifier: UNLICENSED
 pragma solidity >=0.5.0 <0.8.0;
 
 /// @title ERC20EXP Interface
