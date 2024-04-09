0cafb0217bd8212782712b38cb8b460e1361fb19
ADISAKBOONMARK <adisak0661013@gmail.com>
2024-04-09 15:56:42 +0700

    Add unit-test (Mint)

---
+++ test/ERC20EXP/Mint.test.ts
@@ -0,0 +1,39 @@
+export const run = async () => {
+  describe("Mint", async function () {
+    it("[HAPPY] correct mint", async function () {
+      // TODO: add test case (suitable logic and event response).
+    });
+
+    it("[UNHAPPY] mint to zero address", async function () {
+      // TODO: add test case (suitable logic and event response).
+    });
+  });
+
+  describe("Mint To Wholesaler", async function () {
+    it("[HAPPY] correct mint", async function () {
+      // TODO: add test case (suitable logic and event response).
+    });
+
+    it("[UNHAPPY] mint to zero address", async function () {
+      // TODO: add test case (suitable logic and event response).
+    });
+
+    it("[UNHAPPY] mint to non-wholesaler", async function () {
+      // TODO: add test case (suitable logic and event response).
+    });
+  });
+
+  describe("Mint To Retailer", async function () {
+    it("[HAPPY] correct mint", async function () {
+      // TODO: add test case (suitable logic and event response).
+    });
+
+    it("[UNHAPPY] mint to zero address", async function () {
+      // TODO: add test case (suitable logic and event response).
+    });
+
+    it("[UNHAPPY] mint to non-retailer", async function () {
+      // TODO: add test case (suitable logic and event response).
+    });
+  });
+};
