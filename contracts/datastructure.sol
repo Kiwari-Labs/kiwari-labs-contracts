654b74dc93b3830ce9cde6afbdd3ab793193c842
MASDXI <sirawitt42@gmail.com>
2024-05-08 16:20:04 +0700

    more clean up

--- contracts/datastructure.sol
+++ contracts/datastructure.sol
@@ -1,5 +1,7 @@
 pragma solidity >0.8.0;
 
+// [133,119,238,14,147,10,6,62,158,154,27]
+
 contract DataStructure {
     struct List {
         uint256 prev;
@@ -28,28 +30,25 @@ contract DataStructure {
             if (value > tempTail) {
                 _insertTail(value);
             }
+            // logic bug
             // first partition
-            if (value < tempPivot && value > tempHead) {
-                // use tempPivot as temporary tail
-                // TODO code flaw?
-                while (_list[tempHead].next < tempPivot) {
-                    tempHead = _list[tempHead].next;
-                }
-                _list[tempHead].next = value;
-                _list[value].prev = tempHead;
-                _list[value].next = tempPivot;
-            }
-            // update second partition
-            if (value > tempPivot && value < tempTail) {
-                // use tempPivot as temporary head
-                // TODO code flaw?
-                while (_list[tempTail].prev > tempPivot) {
-                    tempTail = _list[tempTail].prev;
-                }
-                _list[tempTail].prev = value;
-                _list[value].next = tempTail;
-                _list[value].prev = tempPivot;
-            }
+            // if (value < tempPivot && value > tempHead) {
+            //     // use tempPivot as temporary tail
+            //     while (_list[tempHead].next < tempPivot) {
+            //         tempHead = _list[tempHead].next;
+            //     }
+            //     _updateNext(tempHead, value);
+            //     _insertNewValue(value, tempHead, tempPivot);
+            // }
+            // // update second partition
+            // if (value > tempPivot && value < tempTail) {
+            //     // use tempPivot as temporary head
+            //     while (_list[tempTail].prev > tempPivot) {
+            //         tempTail = _list[tempTail].prev;
+            //     }
+            //     _updatePrev(tempTail, value);
+            //     _insertNewValue(value, tempPivot, tempHead);
+            // }
             pivot = value;
         }
     }
@@ -63,7 +62,20 @@ contract DataStructure {
     function _insertTail(uint256 value) internal {
         _list[tail].prev = value;
         _list[value].next = tail;
-        head = value;
+        tail = value;
+    }
+
+    function _insertNewValue(uint256 value, uint256 prev, uint256 next) internal {
+        _list[value].prev = prev;
+        _list[value].next = next;
+    }
+
+    function _updatePrev(uint256 value, uint256 newPrev) internal {
+        _list[value].prev = newPrev;
+    }
+
+    function _updateNext(uint256 value, uint256 newNext) internal {
+        _list[value].next = newNext;
     }
 
     function query(uint256 value) public view returns (List memory) {
