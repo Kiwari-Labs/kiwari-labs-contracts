f487c191ac7fd393d08fbd71ca802fa17eab53f1
MASDXI <sirawitt42@gmail.com>
2024-05-09 11:29:46 +0700

    revised code

--- contracts/datastructure.sol
+++ contracts/datastructure.sol
@@ -1,4 +1,4 @@
-pragma solidity >0.8.0;
+pragma solidity ^0.8.0;
 
 contract DoublyLinkedListWithSentinel {
     struct Node {
@@ -6,99 +6,80 @@ contract DoublyLinkedListWithSentinel {
         uint256 next;
     }
 
-    uint8 private constant _sentinel = 0;
+    uint256 private constant _SENTINEL = 0;
 
     uint256 private _head;
     uint256 private _middle;
     uint256 private _tail;
     uint256 private _size;
 
-    // make mapping into list
     mapping(uint256 => Node) private _nodes;
 
     function _insertNode(uint256 index, uint256 prev, uint256 next) private {
         _nodes[index] = Node(prev, next);
     }
 
-    // TODO
-    // function _insertAfter() private {
-    //
-    // }
-
-    // TODO
-    // function _insertBefore() private {
-    //
-    // }
-
-    function _updatePrev(uint256 value, uint256 newPrev) internal {
-        _nodes[value].prev = newPrev;
+    function _updatePrev(uint256 index, uint256 newPrev) private {
+        _nodes[index].prev = newPrev;
     }
 
-    function _updateNext(uint256 value, uint256 newNext) internal {
-        _nodes[value].next = newNext;
+    function _updateNext(uint256 index, uint256 newNext) private {
+        _nodes[index].next = newNext;
     }
 
-    function _updatemiddle(uint256 value) internal {
-        _middle = value;
+    function _insertMiddle(uint256 index) private {
+        _middle = index;
     }
 
-    function _insertHead(uint256 value) internal {
-        _updatePrev(_head, value);
-        _insertNode(value, _sentinel, _head);
-        _head = value;
-        _size++;
+    function _insertHead(uint256 index) private {
+        _updateNext(_SENTINEL, index);
+        _updatePrev(index, _SENTINEL); // Update previous of index to sentinel
+        _updatePrev(_head, index);
+        _insertNode(index, _SENTINEL, _head);
+        _head = index;
     }
 
-    function _insertTail(uint256 value) internal {
-        _updateNext(_tail, value);
-        _insertNode(value, _tail, _sentinel);
-        _tail = value;
-        _size++;
+    function _insertTail(uint256 index) private {
+        _updatePrev(_SENTINEL, index);
+        _updateNext(index, _SENTINEL); // Update next of index to sentinel
+        _updateNext(_tail, index);
+        _insertNode(index, _tail, _SENTINEL);
+        _tail = index;
     }
 
-    /// @custom:overloading-method add multiple index
     function insert(uint256[] memory indexes) public {
         for (uint i = 0; i < indexes.length; i++) {
             insert(indexes[i]);
         }
     }
 
-    /// @custom:overloading-method add single index
     function insert(uint256 value) public {
-        uint256 tmpSize = _size;
-        uint256 tempHead = _head;
-        uint256 tempTail = _tail;
-        uint256 tempmiddle = _middle;
-        if (tmpSize == 0) {
+        if (_size == 0) {
             _insertHead(value);
             _insertTail(value);
-            _middle = value;
+            _insertMiddle(value);
+        } else if (value <= _nodes[_head].next) {
+            _insertHead(value);
+            if (_size % 2 != 0) {
+                _insertMiddle(_head);
+            }
+        } else if (value >= _nodes[_tail].prev) {
+            _insertTail(value);
+            if (_size % 2 == 0) {
+                _insertMiddle(_tail);
+            }
         } else {
-            if (value < tempHead) {
-                _insertHead(value);
+            uint256 current = _head;
+            while (current != _SENTINEL && value > current) {
+                current = _nodes[current].next;
             }
-            if (value > tempTail) {
-                _insertTail(value);
-            }
-            // fix the bug
-            if (value > tempHead && value < tempmiddle) {
-                while (_nodes[tempHead].next < tempmiddle) {
-                    tempHead = _nodes[tempHead].next;
-                }
-                _updateNext(tempHead, value);
-                _insertNode(value, tempHead, tempmiddle);
-                _middle = value;
-            }
-            if (value < tempTail && value > tempmiddle) {
-                while (_nodes[tempTail].prev > tempmiddle) {
-                    tempTail = _nodes[tempTail].prev;
-                }
-                _updatePrev(tempTail, value);
-                _insertNode(value, tempmiddle, tempHead);
-                _middle = value;
+            _insertNode(value, _nodes[current].prev, current);
+            _updateNext(_nodes[current].prev, value);
+            _updatePrev(current, value);
+            if (_size % 2 == 0) {
+                _insertMiddle(_nodes[_middle].next);
             }
         }
-        // avoid size increase if duplicate in list.
         _size++;
     }
 
@@ -114,60 +95,32 @@ contract DoublyLinkedListWithSentinel {
         return _tail;
     }
 
-    function sentinel() public view returns (Node memory) {
-        return _nodes[_sentinel];
+    function sentinel() public pure returns (uint256) {
+        return _SENTINEL;
     }
 
     function node(uint256 index) public view returns (Node memory) {
         return _nodes[index];
     }
 
-    function ascendingList() public view returns (uint256 [] memory) {
-        uint256 index = _sentinel;
-        uint256 tmpSize = _size;
-        uint256[] memory asd = new uint256[](tmpSize);
-        for (uint256 i = 0; i < tmpSize; i++) {
-            asd[i] = _nodes[index].next;
+    function ascendingList() public view returns (uint256[] memory) {
+        uint256 index = _head;
+        uint256[] memory asc = new uint256[](_size);
+        for (uint256 i = 0; i < _size; i++) {
+            asc[i] = index;
             index = _nodes[index].next;
         }
-        return asd;
+        return asc;
     }
 
-    function descendingList() public view  returns (uint256 [] memory) {
-        uint256 index = _sentinel;
-        uint256 tmpSize = _size;
-        uint256[] memory des = new uint256[](tmpSize);
-        for (uint256 i = 0; i < tmpSize; i++) {
-            des[i] = _nodes[index].prev;
+    function descendingList() public view returns (uint256[] memory) {
+        uint256 index = _tail;
+        uint256[] memory des = new uint256[](_size);
+        for (uint256 i = 0; i < _size; i++) {
+            des[i] = index;
             index = _nodes[index].prev;
         }
         return des;
-    }
-
-    // TODO refactor to determistic size array would be greate.
-    function firstParitionList() public view returns (uint256 [] memory) {
-       uint256 index = _sentinel;
-       uint256[] memory part;
-       uint256 i;
-       while (_nodes[index].next != _middle) {
-            part[i] = index;
-            index = _nodes[index].next;
-            i++;
-       }
-       return part;
-    }
-
-    // TODO refactor to determistic size array would be greate.
-    function secondPartitionList() public view returns (uint256 [] memory) {
-        uint256 index = _sentinel;
-        uint256[] memory part;
-        uint256 i;
-        while (_nodes[index].prev != _middle) {
-            part[i] = index;
-            index = _nodes[index].prev;
-            i++;
-        }
-        return part;
     }
 
     function size() public view returns (uint256) {
