497da08097e7ab6405b3ccc62f19209732daa9e2
MASDXI <sirawitt42@gmail.com>
2024-06-27 14:10:56 +0700

    Update test case transfer across slot.

--- test/LightWeightERC20EXP/Transfer.test.ts
+++ test/LightWeightERC20EXP/Transfer.test.ts
@@ -1,59 +1,107 @@
-import {expect} from "chai";
-import {ethers, network} from "hardhat";
-import {deployLightWeightERC20EXP} from "../utils.test";
-import {ZERO_ADDRESS} from "../constant.test";
-import {mine, mineUpTo, time} from "@nomicfoundation/hardhat-network-helpers";
+import { expect } from "chai";
+import { deployLightWeightERC20EXP } from "../utils.test";
+import { ZERO_ADDRESS } from "../constant.test";
+import { parseEther } from "ethers/lib/utils";
+import { network } from "hardhat";
+import { mineUpTo } from "@nomicfoundation/hardhat-network-helpers";
 
 export const run = async () => {
   describe("Transfer", async function () {
     it("[HAPPY] correct transfer", async function () {
-      // TODO: add test case (suitable logic and event response).
-      const {erc20exp, alice, bob} = await deployLightWeightERC20EXP();
+      const { erc20exp, alice, bob } = await deployLightWeightERC20EXP();
+
+      const oneEther = parseEther("1.0");
       const aliceAddress = await alice.getAddress();
       const bobAddress = await bob.getAddress();
-      const balanceAliceBefore = await erc20exp["balanceOf(address)"](aliceAddress);
-      await expect(await erc20exp.mintRetail(aliceAddress, 100n))
+
+      const balanceAliceBeforeMint = await erc20exp["balanceOf(address)"](aliceAddress);
+      await expect(await erc20exp.mintRetail(aliceAddress, oneEther))
         .to.emit(erc20exp, "Transfer")
-        .withArgs(ZERO_ADDRESS, aliceAddress, 100n);
-      const balanceAliceAfter = await erc20exp["balanceOf(address)"](aliceAddress);
-      expect(balanceAliceBefore).to.equal(0);
-      expect(balanceAliceAfter).to.equal(100n);
-      for (let index = 100; index > 0; index--) {
-        await erc20exp.connect(alice).transfer(bobAddress, 1n);
-      }
-      // const balanceAliceAfter2 = await erc20exp["balanceOf(address)"](aliceAddress);
-      // console.log("🚀 ~ balanceAliceAfter2:", balanceAliceAfter2)
-      // const balanceAliceList = await erc20exp.tokenList(aliceAddress,0n,0n);
-      // console.log("🚀 ~ balanceBobList:", balanceAliceList)
-      // const balanceBob = await erc20exp["balanceOf(address)"](bobAddress);
-      // console.log("balanceBob", balanceBob);
-      // expect(balanceBob).to.equal(100);
+        .withArgs(ZERO_ADDRESS, aliceAddress, oneEther);
+      const balanceAliceAfterMint = await erc20exp["balanceOf(address)"](aliceAddress);
+      expect(balanceAliceBeforeMint).to.equal(0);
+      expect(balanceAliceAfterMint).to.equal(oneEther);
+
+      await expect(erc20exp.connect(alice).transfer(bobAddress, oneEther))
+        .to.emit(erc20exp, "Transfer")
+        .withArgs(aliceAddress, bobAddress, oneEther);
+      const balanceBob = await erc20exp["balanceOf(address)"](bobAddress);
+      expect(balanceBob).to.equal(oneEther);
     });
 
     it("[HAPPY] correct bulk transfer", async function () {
-      // TODO: add test case (suitable logic and event response).
-      const {erc20exp, alice, bob} = await deployLightWeightERC20EXP();
+      const { erc20exp, alice, bob } = await deployLightWeightERC20EXP();
+
+      const oneEther = parseEther("1.0");
+      const bulkEther = parseEther("273.0");
       const aliceAddress = await alice.getAddress();
       const bobAddress = await bob.getAddress();
-      const balanceAliceBefore = await erc20exp["balanceOf(address)"](aliceAddress);
-      for (let index = 0; index < 270; index++) {
-        await expect(await erc20exp.mintRetail(aliceAddress, 1n))
+
+      const balanceAliceBeforeMint = await erc20exp["balanceOf(address)"](aliceAddress);
+      for (let index = 0; index < 273; index++) {
+        await expect(await erc20exp.mintRetail(aliceAddress, oneEther))
           .to.emit(erc20exp, "Transfer")
-          .withArgs(ZERO_ADDRESS, aliceAddress, 1n);
+          .withArgs(ZERO_ADDRESS, aliceAddress, oneEther);
       }
-      const balanceAliceAfter = await erc20exp["balanceOf(address)"](aliceAddress);
-      expect(balanceAliceBefore).to.equal(0);
-      expect(balanceAliceAfter).to.equal(270n);
-      await erc20exp.connect(alice).transfer(bobAddress, 270n);
-      const balanceBob = await erc20exp["balanceOf(address)"](bobAddress);
+      const balanceAliceAfterMint = await erc20exp["balanceOf(address)"](aliceAddress);
+      expect(balanceAliceBeforeMint).to.equal(0);
+      expect(balanceAliceAfterMint).to.equal(bulkEther);
+
+      await expect(await erc20exp.connect(alice).transfer(bobAddress, bulkEther))
+        .to.emit(erc20exp, "Transfer")
+        .withArgs(aliceAddress, bobAddress, bulkEther);
       const balanceAliceAfterTransfer = await erc20exp["balanceOf(address)"](aliceAddress);
-      console.log("balanceAliceAfterTransfer:", balanceAliceAfterTransfer);
-      const bobTokenList = await erc20exp.tokenList(bobAddress, 0n, 0n);
-      console.log("bobTokenList:", bobTokenList.length);
-      console.log("balanceBob", balanceBob);
+      const balanceBob = await erc20exp["balanceOf(address)"](bobAddress);
       const aliceTokenList = await erc20exp.tokenList(aliceAddress, 0n, 0n);
-      console.log("aliceTokenList:", aliceTokenList.length);
-      expect(balanceBob).to.equal(270);
+      const bobTokenList = await erc20exp.tokenList(bobAddress, 0n, 0n);
+
+      expect(balanceBob).to.equal(bulkEther);
+      expect(bobTokenList.length).to.equal(273);
+      expect(aliceTokenList.length).to.equal(0);
+      expect(balanceAliceAfterTransfer).to.equal(0);
+    });
+
+    it("[HAPPY] correct bulk transfer across slot", async function () {
+      await network.provider.request({
+        method: "hardhat_reset",
+        params: [],
+      });
+      const { erc20exp, alice, bob } = await deployLightWeightERC20EXP();
+
+      const oneEther = parseEther("1.0");
+      const twoEther = parseEther("2.0");
+      const aliceAddress = await alice.getAddress();
+      const bobAddress = await bob.getAddress();
+
+      await expect(await erc20exp.mintRetail(aliceAddress, oneEther))
+        .to.emit(erc20exp, "Transfer")
+        .withArgs(ZERO_ADDRESS, aliceAddress, oneEther);
+
+      let currentEraAndSlot = await erc20exp.currentEraAndSlot();
+      expect(currentEraAndSlot.era).to.equal(0);
+      expect(currentEraAndSlot.slot).to.equal(0);
+
+      mineUpTo((await erc20exp.blockPerSlot()) + 9); // rounding error bypass.
+
+      currentEraAndSlot = await erc20exp.currentEraAndSlot();
+      expect(currentEraAndSlot.era).to.equal(0);
+      expect(currentEraAndSlot.slot).to.equal(1);
+
+      await expect(await erc20exp.mintRetail(aliceAddress, oneEther))
+        .to.emit(erc20exp, "Transfer")
+        .withArgs(ZERO_ADDRESS, aliceAddress, oneEther);
+
+      const balanceAlice = await erc20exp["balanceOf(address)"](aliceAddress);
+      expect(balanceAlice).to.equal(twoEther);
+      await expect(await erc20exp.connect(alice).transfer(bobAddress, twoEther))
+        .to.emit(erc20exp, "Transfer")
+        .withArgs(aliceAddress, bobAddress, twoEther);
+      const balanceBob = await erc20exp["balanceOf(address)"](bobAddress);
+      expect(balanceBob).to.equal(twoEther);
+    });
+
+    it("[HAPPY] correct bulk transfer across era", async function () {
+      // TODO: add test case (suitable logic and event response).
     });
 
     it("[HAPPY] correct transferFrom", async function () {
