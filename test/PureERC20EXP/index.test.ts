4cef49bdf41ae2b632cb72cf06ca13f95e27f25a
ADISAKBOONMARK <adisak0661013@gmail.com>
2024-07-21 17:49:10 +0700

    Add test cases general

--- test/PureERC20EXP/index.test.ts
+++ test/PureERC20EXP/index.test.ts
@@ -1,9 +1,11 @@
+import * as General from "./General.test";
 import * as Mint from "./Mint.test";
 import * as Burn from "./Burn.test";
 import * as Transfer from "./Transfer.test";
 
 export const run = async () => {
-  describe.only("ERC20EXP", async function () {
+  describe("ERC20EXP", async function () {
+    General.run();
     Mint.run();
     Burn.run();
     Transfer.run();
