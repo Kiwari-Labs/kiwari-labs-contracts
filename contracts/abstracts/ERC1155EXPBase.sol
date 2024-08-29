aee277072e68c5e15ef502b7c48c3878b16c49b9
MASDXI <sirawitt42@gmail.com>
2024-08-29 15:31:30 +0700

    chore: abstract contract multiple type of asset
    Signed-off-by: MASDXI <sirawitt42@gmail.com>

--- contracts/abstracts/ERC1155EXPBase.sol
+++ contracts/abstracts/ERC1155EXPBase.sol
@@ -4,9 +4,13 @@ pragma solidity >=0.5.0 <0.9.0;
 /// @title ERC1155EXP Base abstract contract
 /// @author Kiwari Labs
 
-// @TODO
+// import {AssetStamp} from "../libraries/AssetStamp.sol";
 // import {IERC1155} form "@openzeppelin/contracts/token/ERC1155/extensions/IERC1155.sol";
+// import {IERC1155MetadataURI} from "@openzeppelin/contracts/token/ERC1155/extensions/IERC1155MetadataURI.sol";
 
 abstract contract ERC1155EXPBase {
+    // using AssetStamp for uint256;
+
+    
     // @TODO
 } 
--- contracts/abstracts/ERC721EXPBase.sol
+++ contracts/abstracts/ERC721EXPBase.sol
@@ -4,9 +4,27 @@ pragma solidity >=0.5.0 <0.9.0;
 /// @title ERC721EXP Base abstract contract
 /// @author Kiwari Labs
 
-// @TODO
-// import {IERC721} form "@openzeppelin/contracts/token/ERC1155/extensions/IERC721.sol";
+// import {AssetStamp} from "../libraries/AssetStamp.sol";
+// import {IERC721} form "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+// import {IERC721Enumerable} "@openzeppelin/contracts/token/ERC721/extensions/IERC721Enumerable.sol";
+// import {IERC721Metadata} from "@openzeppelin/contracts/token/ERC721/extensions/IERC721Metadata.sol";
 
 abstract contract ERC721EXPBase {
+    // using AssetStamp for uint256;
+
+    // function mint(address account) internal {
+    //      _mint();
+    //      tokenId.stampSet(_blockNumberProvider());
+    // }
+
+    // function burn(uint256 tokenId) internal {
+    //      _burn();
+    //      tokenId.stampClear();
+    //}
+
+    // function update(uint256 blockNumber) internal {
+    //      tokenId.stampUpdate(blockNumber);
+    //}
+
     // @TODO
 } 
