97d651d3af204b5efac19bcd6bb7e928fbd75a00
MASDXI <sirawitt42@gmail.com>
2024-08-29 11:31:36 +0700

    feat: starting adding ERC721 and ERC1155 to the abstract
    Signed-off-by: MASDXI <sirawitt42@gmail.com>

---
+++ contracts/abstracts/ERC1155EXPBase.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: BUSL-1.1
+pragma solidity >=0.5.0 <0.9.0;
+
+/// @title ERC1155EXP Base abstract contract
+/// @author Kiwari Labs
+
+// @TODO
+// import {IERC1155} form "@openzeppelin/contracts/token/ERC1155/extensions/IERC1155.sol";
+
+abstract contract ERC1155EXPBase {
+    // @TODO
+} 
---
+++ contracts/abstracts/ERC721EXPBase.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: BUSL-1.1
+pragma solidity >=0.5.0 <0.9.0;
+
+/// @title ERC721EXP Base abstract contract
+/// @author Kiwari Labs
+
+// @TODO
+// import {IERC721} form "@openzeppelin/contracts/token/ERC1155/extensions/IERC721.sol";
+
+abstract contract ERC721EXPBase {
+    // @TODO
+} 
