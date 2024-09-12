18f243072a769c4da46cf021eaa977322f5f182e
MASDXI <sirawitt42@gmail.com>
2024-09-12 14:52:03 +0700

    fix: update test case transfer whitelist
    Signed-off-by: MASDXI <sirawitt42@gmail.com>

--- test/extensions/ERC20EXPWhitelist/Transfer.test.ts
+++ test/extensions/ERC20EXPWhitelist/Transfer.test.ts
@@ -1,100 +1,72 @@
-import {expect} from "chai";
-import {deployERC20EXPWhitelist} from "../../utils.test";
-import {ZERO_ADDRESS} from "../../constant.test";
-import {parseEther} from "ethers/lib/utils";
-import {reset, time, mineUpTo} from "@nomicfoundation/hardhat-network-helpers";
+import { expect } from "chai";
+import { deployERC20EXPWhitelist } from "../../utils.test";
+import { ZERO_ADDRESS } from "../../constant.test";
+import { parseEther } from "ethers/lib/utils";
+import { reset, time, mineUpTo } from "@nomicfoundation/hardhat-network-helpers";
 
 export const run = async () => {
-  // describe("Transfer", async function () {
-  //   it("[HAPPY] correct transfer", async function () {
-  //     const {erc20exp, alice, bob} = await deployERC20EXPWhitelist();
-  //     const oneEther = parseEther("1.0");
-  //     const aliceAddress = await alice.getAddress();
-  //     const bobAddress = await bob.getAddress();
-  //     const balanceAliceBeforeMint = await erc20exp["balanceOf(address)"](aliceAddress);
-  //     await expect(await erc20exp.mintRetail(aliceAddress, oneEther))
-  //       .to.emit(erc20exp, "Transfer")
-  //       .withArgs(ZERO_ADDRESS, aliceAddress, oneEther);
-  //     const balanceAliceAfterMint = await erc20exp["balanceOf(address)"](aliceAddress);
-  //     expect(balanceAliceBeforeMint).to.equal(0);
-  //     expect(balanceAliceAfterMint).to.equal(oneEther);
-  //     await expect(erc20exp.connect(alice).transfer(bobAddress, oneEther))
-  //       .to.emit(erc20exp, "Transfer")
-  //       .withArgs(aliceAddress, bobAddress, oneEther);
-  //     const balanceBob = await erc20exp["balanceOf(address)"](bobAddress);
-  //     expect(balanceBob).to.equal(oneEther);
-  //   });
-  //   it("[HAPPY] correct bulk transfer", async function () {
-  //     const {erc20exp, alice, bob} = await deployERC20EXPWhitelist();
-  //     const oneEther = parseEther("1.0");
-  //     const bulkEther = parseEther("273.0");
-  //     const aliceAddress = await alice.getAddress();
-  //     const bobAddress = await bob.getAddress();
-  //     const balanceAliceBeforeMint = await erc20exp["balanceOf(address)"](aliceAddress);
-  //     for (let index = 0; index < 273; index++) {
-  //       await expect(await erc20exp.mintRetail(aliceAddress, oneEther))
-  //         .to.emit(erc20exp, "Transfer")
-  //         .withArgs(ZERO_ADDRESS, aliceAddress, oneEther);
-  //     }
-  //     const balanceAliceAfterMint = await erc20exp["balanceOf(address)"](aliceAddress);
-  //     expect(balanceAliceBeforeMint).to.equal(0);
-  //     expect(balanceAliceAfterMint).to.equal(bulkEther);
-  //     await expect(await erc20exp.connect(alice).transfer(bobAddress, bulkEther))
-  //       .to.emit(erc20exp, "Transfer")
-  //       .withArgs(aliceAddress, bobAddress, bulkEther);
-  //     const balanceAliceAfterTransfer = await erc20exp["balanceOf(address)"](aliceAddress);
-  //     const balanceBob = await erc20exp["balanceOf(address)"](bobAddress);
-  //     const aliceTokenList = await erc20exp.tokenList(aliceAddress, 0n, 0n);
-  //     const bobTokenList = await erc20exp.tokenList(bobAddress, 0n, 0n);
-  //     expect(balanceBob).to.equal(bulkEther);
-  //     expect(bobTokenList.length).to.equal(273);
-  //     expect(aliceTokenList.length).to.equal(0);
-  //     expect(balanceAliceAfterTransfer).to.equal(0);
-  //   });
-  //   it("[HAPPY] correct bulk transfer across slot", async function () {
-  //     await reset();
-  //     expect(await time.latestBlock()).to.equal(0);
-  //     const {erc20exp, alice, bob} = await deployERC20EXPWhitelist();
-  //     const oneEther = parseEther("1.0");
-  //     const twoEther = parseEther("2.0");
-  //     const aliceAddress = await alice.getAddress();
-  //     const bobAddress = await bob.getAddress();
-  //     await expect(await erc20exp.mintRetail(aliceAddress, oneEther))
-  //       .to.emit(erc20exp, "Transfer")
-  //       .withArgs(ZERO_ADDRESS, aliceAddress, oneEther);
-  //     let currentEraAndSlot = await erc20exp.currentEraAndSlot();
-  //     expect(currentEraAndSlot.era).to.equal(0);
-  //     expect(currentEraAndSlot.slot).to.equal(0);
-  //     await mineUpTo((await erc20exp.getBlockPerSlot()) + 1);
-  //     currentEraAndSlot = await erc20exp.currentEraAndSlot();
-  //     expect(currentEraAndSlot.era).to.equal(0);
-  //     expect(currentEraAndSlot.slot).to.equal(1);
-  //     await expect(await erc20exp.mintRetail(aliceAddress, oneEther))
-  //       .to.emit(erc20exp, "Transfer")
-  //       .withArgs(ZERO_ADDRESS, aliceAddress, oneEther);
-  //     const balanceAlice = await erc20exp["balanceOf(address)"](aliceAddress);
-  //     expect(balanceAlice).to.equal(twoEther);
-  //     await expect(await erc20exp.connect(alice).transfer(bobAddress, twoEther))
-  //       .to.emit(erc20exp, "Transfer")
-  //       .withArgs(aliceAddress, bobAddress, twoEther);
-  //     const balanceBob = await erc20exp["balanceOf(address)"](bobAddress);
-  //     expect(balanceBob).to.equal(twoEther);
-  //   });
-  //   it("[HAPPY] correct bulk transfer across era", async function () {
-  //     // TODO: add test case (suitable logic and event response).
-  //   });
-  //   it("[HAPPY] correct transferFrom", async function () {
-  //     // TODO: add test case (suitable logic and event response).
-  //   });
-  //   it("[UNHAPPY] should not be transfer to zero address", async function () {
-  //     // TODO: add test case (suitable logic and event response).
-  //   });
-  //   it("[UNHAPPY] insufficient balance", async function () {
-  //     // TODO: add test case (suitable logic and event response).
-  //   });
-  //   it("[UNHAPPY] should not transferable the token that expired", async function () {
-  //     // TODO: add test case (suitable logic and event response).
-  //   });
-  //   // In cases of Wholesale and Retail are still in the designing phase to be discussed later.
-  // });
+  describe("Transfer", async function () {
+    it("[HAPPY] correct bulk transfer across era", async function () {
+      // TODO: add test case (suitable logic and event response).
+    });
+    it("[HAPPY] correct transferFrom", async function () {
+      // TODO: add test case (suitable logic and event response).
+    });
+    it("[UNHAPPY] should not be transfer to zero address", async function () {
+      // TODO: add test case (suitable logic and event response).
+    });
+    it("[UNHAPPY] insufficient balance", async function () {
+      // TODO: add test case (suitable logic and event response).
+    });
+    it("[UNHAPPY] should not transferable the token that expired", async function () {
+      // TODO: add test case (suitable logic and event response).
+    });
+    // In cases of Wholesale and Retail are still in the designing phase to be discussed later.
+    it("[HAPPY] whitelist address transfer spendable balance to non whitelist address", async function () {
+      const { erc20exp, deployer, alice, bob } = await deployERC20EXPWhitelist();
+      const aliceAddress = await alice.getAddress();
+      const bobAddress = await bob.getAddress();
+      const deployerAddress = await deployer.getAddress();
+      await expect(erc20exp.grantWhitelist(aliceAddress))
+        .to.emit(erc20exp, "WhitelistGranted")
+        .withArgs(deployerAddress, aliceAddress);
+      await expect(erc20exp.mintSpendableWhitelist(aliceAddress, 1))
+        .to.be.emit(erc20exp, "Transfer")
+        .withArgs(ZERO_ADDRESS, aliceAddress, 1);
+      await expect(erc20exp.connect(alice).transfer(bobAddress, 1))
+        .to.be.emit(erc20exp, "Transfer")
+        .withArgs(aliceAddress, ZERO_ADDRESS, 1)
+        .to.be.emit(erc20exp, "Transfer")
+        .withArgs(ZERO_ADDRESS, bobAddress, 1);
+      expect(await erc20exp.balanceOf(aliceAddress)).equal(0);
+      expect(await erc20exp.balanceOf(bobAddress)).equal(1);
+    });
+
+    it("[HAPPY] whitelist address transfer spendable balance to whitelist address", async function () {
+      const { erc20exp, deployer, alice, bob } = await deployERC20EXPWhitelist();
+      const aliceAddress = await alice.getAddress();
+      const bobAddress = await bob.getAddress();
+      const deployerAddress = await deployer.getAddress();
+      await expect(erc20exp.grantWhitelist(aliceAddress))
+        .to.emit(erc20exp, "WhitelistGranted")
+        .withArgs(deployerAddress, aliceAddress);
+      await expect(erc20exp.grantWhitelist(bobAddress))
+        .to.emit(erc20exp, "WhitelistGranted")
+        .withArgs(deployerAddress, bobAddress);
+      await expect(erc20exp.mintSpendableWhitelist(aliceAddress, 1))
+        .to.be.emit(erc20exp, "Transfer")
+        .withArgs(ZERO_ADDRESS, aliceAddress, 1);
+      await expect(erc20exp.connect(alice).transfer(bobAddress, 1))
+        .to.be.emit(erc20exp, "Transfer")
+        .withArgs(aliceAddress, bobAddress, 1);
+      expect(await erc20exp.balanceOf(aliceAddress)).equal(0);
+      expect(await erc20exp.balanceOf(bobAddress)).equal(1);
+    });
+
+    it("[UNHAPPY] whitelist address transfer un-spendable balance to whitelist address", async function () {
+    });
+
+    it("[UNHAPPY] whitelist address transfer un-spendable balance to non whitelist address", async function () {
+    });
+  });
 };
