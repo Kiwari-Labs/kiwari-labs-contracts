2a56390212a8852a46d1d1c532d83649ac34c93c
MASDXI <sirawitt42@gmail.com>
2024-09-12 23:06:23 +0700

    test: add test case for check is whitelist address
    Signed-off-by: MASDXI <sirawitt42@gmail.com>

--- test/extensions/ERC20EXPWhitelist/Mint.test.ts
+++ test/extensions/ERC20EXPWhitelist/Mint.test.ts
@@ -1,5 +1,5 @@
-import {expect} from "chai";
-import {deployERC20EXPWhitelist} from "../../utils.test";
+import { expect } from "chai";
+import { deployERC20EXPWhitelist } from "../../utils.test";
 import {
   ERC20_INSUFFICIENT_BALANCE,
   ERC20_INVALID_RECEIVER,
@@ -13,7 +13,7 @@ import {
 export const run = async () => {
   describe("Mint", async function () {
     it("[HAPPY] correct mint spendable to whitelist address", async function () {
-      const {erc20exp, deployer, alice} = await deployERC20EXPWhitelist();
+      const { erc20exp, deployer, alice } = await deployERC20EXPWhitelist();
       const aliceAddress = await alice.getAddress();
       const deployerAddress = await deployer.getAddress();
       await expect(erc20exp.grantWhitelist(aliceAddress))
@@ -22,12 +22,13 @@ export const run = async () => {
       await expect(erc20exp.mintSpendableWhitelist(aliceAddress, 1))
         .to.be.emit(erc20exp, EVENT_TRANSFER)
         .withArgs(ZERO_ADDRESS, aliceAddress, 1);
+      expect(await erc20exp.whitelist(aliceAddress)).equal(true);
       expect(await erc20exp.balanceOf(aliceAddress)).equal(1);
       expect(await erc20exp.safeBalanceOf(aliceAddress)).equal(1);
     });
 
     it("[HAPPY] correct mint un-spendable to whitelist address", async function () {
-      const {erc20exp, deployer, alice} = await deployERC20EXPWhitelist();
+      const { erc20exp, deployer, alice } = await deployERC20EXPWhitelist();
       const aliceAddress = await alice.getAddress();
       const deployerAddress = await deployer.getAddress();
       await expect(erc20exp.grantWhitelist(aliceAddress))
@@ -36,26 +37,29 @@ export const run = async () => {
       await expect(erc20exp.mintUnspendableWhitelist(aliceAddress, 1))
         .to.be.emit(erc20exp, EVENT_TRANSFER)
         .withArgs(ZERO_ADDRESS, aliceAddress, 1);
+      expect(await erc20exp.whitelist(aliceAddress)).equal(true);
       expect(await erc20exp.balanceOf(aliceAddress)).equal(1);
       expect(await erc20exp.safeBalanceOf(aliceAddress)).equal(0);
     });
 
     it("[UNHAPPY] cannot mint to spendable to non whitelist address", async function () {
-      const {erc20exp, alice} = await deployERC20EXPWhitelist();
+      const { erc20exp, alice } = await deployERC20EXPWhitelist();
       const aliceAddress = await alice.getAddress();
       await expect(erc20exp.mintSpendableWhitelist(aliceAddress, 1)).to.be.revertedWithCustomError(
         erc20exp,
         INVALID_WHITELIST_ADDRESS,
       );
+      expect(await erc20exp.whitelist(aliceAddress)).equal(false);
     });
 
     it("[UNHAPPY] cannot mint to un-spendable to non whitelist address", async function () {
-      const {erc20exp, alice} = await deployERC20EXPWhitelist();
+      const { erc20exp, alice } = await deployERC20EXPWhitelist();
       const aliceAddress = await alice.getAddress();
       await expect(erc20exp.mintUnspendableWhitelist(aliceAddress, 1)).to.be.revertedWithCustomError(
         erc20exp,
         INVALID_WHITELIST_ADDRESS,
       );
+      expect(await erc20exp.whitelist(aliceAddress)).equal(false);
     });
   });
 };
