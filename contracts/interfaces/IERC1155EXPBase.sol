86c636c6d66789813595482bcc3501d106fc4dc0
MASDXI <sirawitt42@gmail.com>
2024-09-18 14:05:27 +0700

    chore: add interface IERC1155EXPBase
    Signed-off-by: MASDXI <sirawitt42@gmail.com>

--- contracts/interfaces/IERC1155EXPBase.sol
+++ contracts/interfaces/IERC1155EXPBase.sol
@@ -2,5 +2,9 @@
 pragma solidity >=0.8.0 <0.9.0;
 
 interface IERC1155EXPBase {
-    // @TODO
+    /// @notice Checks if the token with the given `tokenId` has expired.
+    /// @dev This function returns `true` if the token has passed its expiration period.
+    /// @param tokenId The ID of the token to check for expiration.
+    /// @return bool Returns `true` if the token has expired, otherwise `false`.
+    function hasExpired(uint256 tokenId) external view returns (bool);
 }
