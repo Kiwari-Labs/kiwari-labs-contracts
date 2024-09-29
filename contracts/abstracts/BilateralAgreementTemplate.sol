a0b666e2446699f4b682055520cc0a7516ebfe80
MASDXI <sirawitt42@gmail.com>
2024-09-29 23:42:52 +0700

    refactor: BilateralAgreementTemplate with multisig approach
    Signed-off-by: MASDXI <sirawitt42@gmail.com>

--- contracts/abstracts/BilateralAgreementTemplate.sol
+++ contracts/abstracts/BilateralAgreementTemplate.sol
@@ -1,195 +1,240 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.8.0 <0.9.0;
 
-import "../interfaces/IAgreement.sol";
-
 /// @title Bilateral Agreement Template
 /// @author Kiwari Labs
+/// @dev Bilateral Agreement Template applied multi-signature approach
 /// @notice Before approving the agreement make sure each party deposit token meet the requirement.
 
-// Diagram Highlevel of Smart Contract
-//                 +----------------------+        +-----------------+
-//                 |                      |        |                 | ([] approve)
-//    Party ------>|  Bilateral Contract  |------->|   Agreement A   | ([] reject)
-//                 |                      |   |    |    (V1.0.0)     |
-//                 +----------------------+   |    +-----------------+
-//                                            |    +-----------------+
-//                                            |    |                 | ([] approve)
-//                                            |--->|   Agreement B   | ([] reject)
-//                                                 |     (V1.0.1)    |
-//                                                 +-----------------+
+// Highlevel diagram of the smart contract
+//    +----------------------+        +-----------------+
+//    |                      |        |                 |
+//    |  Bilateral Contract  |------->|   Agreement A   |
+//    |                      |   |    |    (V1.0.0)     |
+//    +----------------------+   |    +-----------------+
+//                               |    +-----------------+
+//                               |    |                 |
+//                               |--->|   Agreement B   |
+//                                    |     (V1.0.1)    |
+//                                    +-----------------+
 
-// - initialized(); // for constructor, initialized contract
-// - agreeementName(); // return name of agreement contract
-// - agreementVersion(); // return version of agreement contract
-// - voteForApprove(bool vote); // approve agreement choice (approve or reject)
-// - voteForChange(bool vote);  // change agreement contract
-// - proposeForChange(address contractAddress); // propose for change contract agreement
-// - submit(bytes params); // submit parameter for agreement
-// - parameter(address party); // parameter of party
-// - refund(); // only available if the agreement not passed claim()?
-
+import "../interfaces/IAgreement.sol";
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import {Context} from "@openzeppelin/contracts/utils/Context.sol";
 
 abstract contract BilateralAgreementTemplate is Context {
-    enum STATE {
-        NULL,
-        APPROVED,
-        REJECTED
+    enum TRANSACTION_TYPE {
+        DEFAULT,
+        LOGIC_CHANGE
     }
 
-    struct Party {
-        address account;
-        STATE approve;
-        bytes data;
+    struct Transaction {
+        TRANSACTION_TYPE transactionType;
+        bool executed;
+        uint8 confirmations;
+        bytes[2] data;
     }
 
-    bool private _init;
-    IAgreement private _agreementContract;
-    Party[2] _party;
+    bool private _initialized;
+    IAgreement private _implemetation;
+    Transaction[] private _transactions;
+    address[2] _parties;
+    mapping(uint256 => mapping(address => bool)) private _transactionConfirmed;
 
-    /// @notice Event
-    event InitializedAgreement();
-    event AgreementApproved(address party);
-    event AgreementContractUpdate(address oldContract, address newContract);
-    event SubmitAgreementParameter(address party);
-    event Refunded(address party, address token, uint256 amount);
+    event Initialized();
+    event ImplementationUpdated(address oldImplementation, address newImplementation);
+    event TransactionFinalized(uint256 indexed index);
+    event TransactionRecorded(
+        uint256 indexed index,
+        address indexed sender,
+        TRANSACTION_TYPE indexed transactionType,
+        bytes data
+    );
+    event TransactionRejected(uint256 indexed index, address indexed sender);
+    event TransactionRevoked(uint256 indexed index, address indexed sender);
 
     /// @notice Error
-    error AlreadyApproved();
-    error AlreadyInitialized();
+    error ContractInitialized();
     error InvalidPartyAddress();
-    error InvalidPartyZeroAddress();
     error InvalidAgreementAddress();
-    error InvalidAgreementZeroAddress();
-    error ParameterEmpty();
-    error ExecutionFailed();
+    error AddressZero();
 
-    constructor(address[2] memory _parties, IAgreement _agreementImplementation) {
-        _initAgreement(_parties);
-        _updateAgreement(_agreementImplementation);
+    error Unauthorized();
+    error TransactionExecuted();
+    error TransactionExecutionFailed();
+    error TransactionNotExist();
+    error TransactionNotSubmited();
+    error TransactionSubmited();
+
+    modifier transactionWriter(address sender) {
+        if (sender != _parties[0] && sender != _parties[1]) {
+            revert Unauthorized();
+        }
+        _;
     }
 
-    function _action(address sender, STATE types) private {
-        Party[2] memory party = _party;
-        if (sender == party[0].account || sender == party[1].account) {
-            if (sender == party[0].account) {
-                if (party[0].approve == STATE.NULL) {
-                    party[0].approve = types;
-                } else {
-                    revert AlreadyApproved();
-                }
-            } else {
-                if (party[0].approve == STATE.NULL) {
-                    party[0].approve = types;
-                } else {
-                    revert AlreadyApproved();
-                }
-            }
-            emit AgreementApproved(sender);
-        } else {
+    modifier transactionReader(address sender) {
+        // @TODO do we need observer ?
+        // if ((sender != _parties[0] && sender != _parties[1]) && sender != IObserver.observer(sender)) {
+        //     revert Unauthorized();
+        // }
+        _;
+    }
+
+    modifier transactionExist(uint256 index) {
+        if (index < _transactions.length) {
+            revert TransactionNotExist();
+        }
+        _;
+    }
+
+    modifier transactionExecuted(uint256 index) {
+        if (_transactions[index].executed) {
+            revert TransactionExecuted();
+        }
+        _;
+    }
+
+    constructor(address[2] memory parties_, IAgreement _agreementImplementation) {
+        _initialize(parties_);
+        _updateImplementation(address(_agreementImplementation));
+    }
+
+    function _initialize(address[2] memory parties) private {
+        if (_initialized) {
+            revert ContractInitialized();
+        }
+        address partyA = parties[0];
+        address partyB = parties[1];
+        if (partyA == partyB) {
             revert InvalidPartyAddress();
         }
+        if (partyA != address(0) && partyB != address(0)) {
+            revert AddressZero();
+        }
+        _parties[0] = partyA;
+        _parties[1] = partyB;
+        _initialized = true;
+
+        emit Initialized();
     }
 
-    function _initAgreement(address[2] memory _parties) private {
-        if (_init) {
-            revert AlreadyInitialized();
+    function _updateImplementation(address implement) internal {
+        address implementationCache = address(implement);
+        if (implement == address(0)) {
+            revert AddressZero();
         }
-        if (_parties[0] != _parties[1]) {
-            revert InvalidPartyAddress();
-        }
-        if (_parties[0] != address(0) && _parties[1] != address(0)) {
-            revert InvalidPartyZeroAddress();
-        }
-        _party[0].account = _parties[0];
-        _party[1].account = _parties[1];
-        _init = true;
-
-        emit InitializedAgreement();
-    }
-
-    // @TODO if one party not meet the agreement balance of the contract not enough. the transfer maybe stuck
-    // potentially create DoS of smart contract agreement can't be clear
-    function _refundTokens(address sender) private {
-        Party[2] memory party = _party;
-        bytes memory parameterA = party[0].data;
-        bytes memory parameterB = party[1].data;
-
-        (address tokenA, uint256 amountTokenA) = abi.decode(parameterA, (address, uint256));
-        (address tokenB, uint256 amountTokenB) = abi.decode(parameterB, (address, uint256));
-
-        IERC20(tokenA).transfer(party[0].account, amountTokenA);
-        IERC20(tokenB).transfer(party[1].account, amountTokenB);
-
-        // emit Refunded(partyAddress, token, amount);
-    }
-
-    /// @notice do not change or make modified the _execute function below.
-    function _excecute() private {
-        Party[2] memory party = _party;
-        bytes memory parameterA = party[0].data;
-        bytes memory parameterB = party[1].data;
-        (address tokenA, uint256 amountTokenA) = abi.decode(parameterA, (address, uint256));
-        (address tokenB, uint256 amountTokenB) = abi.decode(parameterB, (address, uint256));
-        bool success = _agreementContract.agreement(parameterA, parameterB);
-        if (success) {
-            IERC20(tokenA).transfer(party[1].account, amountTokenA);
-            IERC20(tokenB).transfer(party[0].account, amountTokenB);
-            _clear();
-        } else {
-            revert ExecutionFailed();
-        }
-    }
-
-    function _clear() private {
-        // clear state and parameter.
-        _party[0].approve = STATE.NULL;
-        _party[1].approve = STATE.NULL;
-        _party[0].data = "";
-        _party[1].data = "";
-    }
-
-    function _updateAgreement(IAgreement agreementContract) internal {
-        address oldAgreementContract = address(_agreementContract);
-        address newAgreementContract = address(agreementContract);
-        if (address(agreementContract) == address(0)) {
-            revert InvalidAgreementZeroAddress();
-        }
-        if (oldAgreementContract == newAgreementContract) {
+        if (implementationCache == implement) {
             revert InvalidAgreementAddress();
         }
-        _agreementContract = agreementContract;
-        emit AgreementContractUpdate(oldAgreementContract, newAgreementContract);
+        _implemetation = IAgreement(implement);
+        emit ImplementationUpdated(implementationCache, implement);
     }
 
-    function approve() public {
-        // @TODO check is both parameter agreement from partyA and partyB aleardy paste before approve.
-        address sender = _msgSender();
-        _action(sender, STATE.APPROVED);
-        // if partyA or partyB approved second party execute if approved
+    function _submitTransaction(
+        address sender,
+        TRANSACTION_TYPE transactionType,
+        bytes calldata data
+    ) private transactionWriter(sender) {
+        uint256 transactionLengthCache = _transactions.length;
+        bool transactionCreation = _transactions[transactionLengthCache].executed;
+        uint8 party = sender == _parties[0] ? 0 : 1;
+        if (transactionLengthCache == 0) {
+            transactionCreation = true;
+        }
+        if (transactionCreation) {
+            Transaction memory newTransaction;
+            newTransaction.transactionType = transactionType;
+            newTransaction.confirmations = 1;
+            newTransaction.data[party] = data;
+            _transactions.push(newTransaction);
+            transactionLengthCache = _transactions.length;
+            _transactionConfirmed[transactionLengthCache][sender] = true;
+            (address token, uint256 value) = abi.decode(data, (address, uint256));
+            IERC20(token).transferFrom(sender, address(this), value);
+        } else {
+            transactionLengthCache -= 1;
+            _transactions[transactionLengthCache].data[party] = data;
+            _transactionConfirmed[transactionLengthCache][sender] = true;
+            (address token, uint256 value) = abi.decode(data, (address, uint256));
+            IERC20(token).transferFrom(sender, address(this), value);
+            _excecuteTransaction(transactionLengthCache);
+        }
+        emit TransactionRecorded(transactionLengthCache, sender, transactionType, data);
     }
 
-    function reject() public {
-        // @TODO check is both parameter agreement from partyA and partyB aleardy paste before reject.
-        address sender = _msgSender();
-        _action(sender, STATE.REJECTED);
-        // if partyA or partyB reject second party clear if approved or reject
+    /// @notice there is no retention period, second party can only submit transaction but not possible to revoke.
+    function _revokeTransaction(
+        address sender,
+        uint256 index
+    ) private transactionExist(index) transactionExecuted(index) transactionWriter(sender) {
+        uint8 party = sender == _parties[0] ? 0 : 1;
+        if (_transactionConfirmed[index][sender]) {
+            _transactions[index].confirmations -= 1;
+            if (_transactions[index].transactionType == TRANSACTION_TYPE.DEFAULT) {
+                (address token, uint256 value) = abi.decode(_transactions[index].data[party], (address, uint256));
+                IERC20(token).transfer(sender, value);
+            }
+            _transactions[index].data[party] = abi.encodePacked("");
+            _transactionConfirmed[index][sender] = false;
+            emit TransactionRevoked(index, sender);
+        } else {
+            revert TransactionNotSubmited();
+        }
+    }
+
+    function _rejectTransaction(
+        address sender,
+        uint256 index
+    ) private transactionExist(index) transactionWriter(sender) {
+        // @TODO to reject transaction require at least 1 confirmation
+        if (!_transactionConfirmed[index][sender] && (_transactions[index].confirmations == 1)) {
+            _transactions[index].executed = true;
+            emit TransactionRejected(index, sender);
+        } else {
+            // revert
+        }
+    }
+
+    /// @notice do not change or make modified the _excecuteTransaction function below.
+    function _excecuteTransaction(uint256 index) private transactionExist(index) transactionExecuted(index) {
+        Transaction memory transactionCache = _transactions[index];
+        if (transactionCache.transactionType == TRANSACTION_TYPE.DEFAULT) {
+            bytes memory parameterACache = transactionCache.data[0];
+            bytes memory parameterBCache = transactionCache.data[1];
+            (address tokenA, uint256 amountTokenA) = abi.decode(parameterACache, (address, uint256));
+            (address tokenB, uint256 amountTokenB) = abi.decode(parameterBCache, (address, uint256));
+            bool success = _implemetation.agreement(parameterACache, parameterBCache);
+            if (success) {
+                IERC20(tokenA).transfer(_parties[1], amountTokenA);
+                IERC20(tokenB).transfer(_parties[0], amountTokenB);
+            } else {
+                revert TransactionExecutionFailed();
+            }
+        } else {
+            bytes memory parameterA = transactionCache.data[0];
+            bytes memory parameterB = transactionCache.data[1];
+            address implementationA = abi.decode(parameterA, (address));
+            address implementationB = abi.decode(parameterB, (address));
+            if (implementationA == implementationB) {
+                _updateImplementation(implementationA);
+            } else {
+                revert TransactionExecutionFailed();
+            }
+        }
+        _transactions[index].executed = true;
+        _transactions[index].confirmations = 2;
+        emit TransactionFinalized(index);
+    }
+
+    // onlyReader can retrieve data
+    function transaction(uint256 index) public view returns (Transaction memory) {
+        // @TODO
+        // _getTranasaction(sender, index); // TransactionReader(sender)
     }
 
     function status() public view returns (bool) {
-        Party[2] memory party = _party;
-        if (party[0].approve == STATE.APPROVED || party[1].approve == STATE.APPROVED) {
-            return true;
-        }
-    }
-
-    function claim() public {
-        address sender = _msgSender();
-        // @TODO check if the state of agreement is reject
-        // claim their token back as refunded
-        // _refundTokens(sender);
+        // @TODO
     }
 
     /// @notice Submits a parameter on behalf of a party in the agreement.
@@ -197,41 +242,56 @@ abstract contract BilateralAgreementTemplate is Context {
     ///      If the caller is party A, their `parameter` is stored in `party[0].data`.
     ///      If the caller is party B, their `parameter` is stored in `party[1].data`.
     ///      If the caller is neither party, the transaction is reverted with an `InvalidPartyAddress` error.
-    /// @param parameter The parameter to be submitted by the party.
+    /// @param data The parameter to be submitted by the party.
     /// @custom:requirements The caller must be either `party[0].account` or `party[1].account`.
     /// @custom:events Emits a `SubmitAgreementParameter` event with the sender's address.
     /// @custom:throws InvalidPartyAddress if the caller is not one of the agreement parties.
-    function submit(bytes calldata parameter) public {
+    function approveAgreement(bytes calldata data) public {
         address sender = _msgSender();
-        Party[2] memory party = _party;
-        if (sender == party[0].account) {
-            party[0].data = parameter;
-        } else if (sender == party[1].account) {
-            party[1].data = parameter;
-        } else {
-            revert InvalidPartyAddress();
+        _submitTransaction(sender, TRANSACTION_TYPE.DEFAULT, data);
+    }
+
+    function approveChange(bytes calldata data) public {
+        address sender = _msgSender();
+        _submitTransaction(sender, TRANSACTION_TYPE.LOGIC_CHANGE, data);
+    }
+
+    function revokeTransaction() public {
+        address sender = _msgSender();
+        uint256 transactionLengthCache = _transactions.length;
+        if (transactionLengthCache > 0) {
+            transactionLengthCache -= 1;
         }
-        emit SubmitAgreementParameter(sender);
+        _revokeTransaction(sender, transactionLengthCache);
+    }
+
+    function rejectTransaction() public {
+        address sender = _msgSender();
+        uint256 transactionLengthCache = _transactions.length;
+        if (transactionLengthCache > 0) {
+            transactionLengthCache -= 1;
+        }
+        _rejectTransaction(sender, transactionLengthCache);
     }
 
     /// @notice Returns the address of the agreement contract.
     /// @dev This function retrieves the address of the internal `_agreementContract`.
     /// @return The address of the `_agreementContract`.
-    function agreement() public view returns (address) {
-        return address(_agreementContract);
+    function implementation() public view returns (address) {
+        return address(_implemetation);
     }
 
     /// @notice Returns the name of the agreement contract.
     /// @dev This function calls the `name()` function from the `_agreementContract`.
     /// @return The name of the agreement contract as a string.
-    function agreementName() public view returns (string memory) {
-        return _agreementContract.name();
+    function name() public view returns (string memory) {
+        return _implemetation.name();
     }
 
     /// @notice Returns the version of the agreement contract.
     /// @dev This function calls the `version()` function from the `_agreementContract`.
     /// @return The version number of the agreement contract as an unsigned integer.
     function version() public view returns (uint) {
-        return _agreementContract.version();
+        return _implemetation.version();
     }
 }
