7eb37138b2a950090debef008fa758c874954fd6
Nattapon33998 <nattapon33998@gmail.com>
2024-10-07 01:47:51 +0700

    fix: change version

--- contracts/campain/Campaign.sol
+++ contracts/campain/Campaign.sol
@@ -1,5 +1,5 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+// SPDX-License-Identifier: BUSL-1.1
+pragma solidity >=0.8.0 <0.9.0;
 
 // Interface for the ERC20 token contract with minting functionality
 interface IERC20Mintable {
