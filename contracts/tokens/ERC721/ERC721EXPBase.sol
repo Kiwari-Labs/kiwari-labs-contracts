e564c07a8c852ada45c4b1a132729ddf29c331ff
MASDXI <sirawitt42@gmail.com>
2024-12-18 10:32:06 +0700

    refactor: ERC721EXP base
    Signed-off-by: MASDXI <sirawitt42@gmail.com>

--- contracts/tokens/ERC721/ERC721EXPBase.sol
+++ contracts/tokens/ERC721/ERC721EXPBase.sol
@@ -1,18 +1,18 @@
 // SPDX-License-Identifier: Apache-2.0
 pragma solidity >=0.8.0 <0.9.0;
 
-/// @title ERC721EXP Base abstract contract
+/// @title ERC721EXP Base
 /// @dev ERC721EXP Base contract each token have individual expiration date.
 /// @author Kiwari Labs
-/// @notice it's adding expiration capability to ERC721 of '@openzeppelin/contracts'
 
-import {IERC5007} from "./extensions/IERC5007.sol";
+import {IERC5007Mod} from "./extensions/IERC5007Mod.sol";
 import {ERC721Enumerable} from "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import {IERC165} from "@openzeppelin/contracts/utils/introspection/IERC165.sol";
 
-abstract contract ERC721EXPBase is ERC721Enumerable, IERC5007 {
+abstract contract ERC721EXPBase is ERC721Enumerable, IERC5007Mod {
     struct Timestamp {
-        uint64 start;
-        uint64 end;
+        uint256 startBlock;
+        uint256 endBlock;
     }
 
     mapping(uint256 => Timestamp) private _tokensTimestamp;
@@ -24,11 +24,11 @@ abstract contract ERC721EXPBase is ERC721Enumerable, IERC5007 {
             return false;
         }
         Timestamp memory timestamp = _tokensTimestamp[tokenId];
-        uint64 current = uint64(block.timestamp);
-        if (timestamp.start == 0 && timestamp.end == 0) {
+        uint256 current = block.number;
+        if (timestamp.startBlock == 0 && timestamp.endBlock == 0) {
             // if start and end is 0,0 mean token non-expirable and return true
             return true;
-        } else if (current > timestamp.start || current < timestamp.end) {
+        } else if (current > timestamp.startBlock || current < timestamp.endBlock) {
             // between start and end valid and return true
             return true;
         } else {
@@ -38,11 +38,13 @@ abstract contract ERC721EXPBase is ERC721Enumerable, IERC5007 {
     }
 
     function _updateTimestamp(uint256 tokenId, uint64 start, uint64 end) internal {
-        if (start <= end) {
+        if (start >= end) {
             // revert ERC5007InvalidTime()
         }
-        _tokensTimestamp[tokenId].start = start;
-        _tokensTimestamp[tokenId].end = end;
+        _tokensTimestamp[tokenId].startBlock = start;
+        _tokensTimestamp[tokenId].endBlock = end;
+
+        // emit tokenTimeSet(tokenId, start, end);
     }
 
     function _mintWithTime(address to, uint256 tokenId, uint64 start, uint64 end) internal {
@@ -59,22 +61,20 @@ abstract contract ERC721EXPBase is ERC721Enumerable, IERC5007 {
         }
     }
 
-    /// @notice Returns 0 as there is no actual total supply due to limitation to track on-chain.
-    function totalSupply() public view virtual override returns (uint256) {
-        return 0;
+    /// @inheritdoc IERC5007Mod
+    function startTime(uint256 tokenId) public view virtual override returns (uint256) {
+        return _tokensTimestamp[tokenId].startBlock;
     }
 
-    /// @inheritdoc IERC5007
-    function startTime(uint256 tokenId) public view virtual override returns (uint64) {
-        return _tokensTimestamp[tokenId].start;
-    }
-
-    /// @inheritdoc IERC5007
-    function endTime(uint256 tokenId) public view virtual override returns (uint64) {
-        return _tokensTimestamp[tokenId].end;
+    /// @inheritdoc IERC5007Mod
+    function endTime(uint256 tokenId) public view virtual override returns (uint256) {
+        return _tokensTimestamp[tokenId].endBlock;
     }
 
     // @TODO function support interface
+    function supportsInterface(bytes4 interfaceId) public view virtual override(IERC165, ERC721Enumerable) returns (bool) {
+        return interfaceId == type(IERC5007Mod).interfaceId || super.supportsInterface(interfaceId);
+    }
 
     // @TODO other useful function like isTokenValid
     function isTokenValid(uint256 tokenId) public view returns (bool) {
