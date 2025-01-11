7d5c458e719323e957912d21b913d180073e5b98
MASDXI <sirawitt42@gmail.com>
2025-01-11 11:30:10 +0700

    remove unused file
    Signed-off-by: MASDXI <sirawitt42@gmail.com>

--- test/tokens/index.test.ts
+++ test/tokens/index.test.ts
@@ -1,11 +1,9 @@
 import * as ERC20 from "./ERC20/index.test";
 import * as ERC721 from "./ERC721/index.test";
-import * as ERC1155 from "./ERC1155/index.test";
 
 export const run = async () => {
   describe("tokens", async function () {
     ERC20.run();
     ERC721.run();
-    ERC1155.run();
   });
 };
