bc5e903ac5106a5b27692563c7ca5933cb9afac5
MASDXI <sirawitt42@gmail.com>
2025-01-12 11:35:32 +0700

    test: update test nearest expiry query
    Signed-off-by: MASDXI <sirawitt42@gmail.com>

--- test/tokens/ERC20/extensions/ERC7818NearestExpiryQuery/deployer.test.ts
+++ test/tokens/ERC20/extensions/ERC7818NearestExpiryQuery/deployer.test.ts
@@ -22,15 +22,15 @@ export const deployERC7818NearestExpiryQueryTLSW = async function ({
 } = {}) {
   const [deployer, alice, bob, charlie] = await ethers.getSigners();
   const ERC7818_NEAREST_EXPIRY_QUERY = await ethers.getContractFactory(ERC7818NearestExpiryQueryTLSW.name, deployer);
-  const erc7818Backlist = (await ERC7818_NEAREST_EXPIRY_QUERY.deploy(
+  const erc7818NearestExpiryQuery = (await ERC7818_NEAREST_EXPIRY_QUERY.deploy(
     ERC20.constructor.name,
     ERC20.constructor.symbol,
     secondsPerEpoch,
     windowSize,
   )) as any as MockERC7818NearestExpiryQueryTLSW;
-  await erc7818Backlist.waitForDeployment();
+  await erc7818NearestExpiryQuery.waitForDeployment();
   return {
-    erc7818Backlist,
+    erc7818NearestExpiryQuery,
     deployer,
     alice,
     bob,
@@ -38,21 +38,21 @@ export const deployERC7818NearestExpiryQueryTLSW = async function ({
   };
 };
 
-export const deployERC781NearestExpiryQueryBLSW = async function ({
+export const deployERC7818NearestExpiryQueryBLSW = async function ({
   blocksPerEpoch = constants.DEFAULT_BLOCKS_PER_EPOCH, // 300 blocks
   windowSize = constants.DEFAULT_WINDOW_SIZE, // fixed width window size 2 epoch.
 } = {}) {
   const [deployer, alice, bob, charlie] = await ethers.getSigners();
   const ERC7818_NEAREST_EXPIRY_QUERY = await ethers.getContractFactory(ERC7818NearestExpiryQueryBLSW.name, deployer);
-  const erc7818Backlist = (await ERC7818_NEAREST_EXPIRY_QUERY.deploy(
+  const erc7818NearestExpiryQuery = (await ERC7818_NEAREST_EXPIRY_QUERY.deploy(
     ERC20.constructor.name,
     ERC20.constructor.symbol,
     blocksPerEpoch,
     windowSize,
   )) as any as MockERC7818NearestExpiryQueryBLSW;
-  await erc7818Backlist.waitForDeployment();
+  await erc7818NearestExpiryQuery.waitForDeployment();
   return {
-    erc7818Backlist,
+    erc7818NearestExpiryQuery,
     deployer,
     alice,
     bob,
