5ce0c738c394a735d992508d022fa4c2681c17e0
ADISAKBOONMARK <adisak0661013@gmail.com>
2025-01-12 13:22:46 +0700

    build: add unit-test of suspend extension

---
+++ mocks/contracts/tokens/ERC20/extensions/TLSW/MockERC7818SuspendTLSW.sol
@@ -0,0 +1,65 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity >=0.8.0 <0.9.0;
+
+import {ERC20EXPBase} from "../../../../../../contracts/tokens/ERC20/ERC20EXPBase.sol";
+import {ERC20TLSW} from "../../../../../../contracts/tokens/ERC20/ERC20TLSW.sol";
+import {ERC7818Suspend} from "../../../../../../contracts/tokens/ERC20/extensions/ERC7818Suspend.sol";
+
+contract MockERC7818SuspendTLSW is ERC20TLSW, ERC7818Suspend {
+    constructor(
+        string memory _name,
+        string memory _symbol,
+        uint40 secondsPerEpoch_,
+        uint8 windowSize_
+    ) ERC20TLSW(_name, _symbol, block.timestamp, secondsPerEpoch_, windowSize_, false) {}
+
+    function _epochType() internal pure virtual override(ERC20EXPBase, ERC20TLSW) returns (EPOCH_TYPE) {
+        return super._epochType();
+    }
+
+    function _getEpoch(uint256 pointer) internal view virtual override(ERC20EXPBase, ERC20TLSW) returns (uint256) {
+        return super._getEpoch(pointer);
+    }
+
+    function _getWindowRage(
+        uint256 pointer
+    ) internal view virtual override(ERC20EXPBase, ERC20TLSW) returns (uint256 fromEpoch, uint256 toEpoch) {
+        return super._getWindowRage(pointer);
+    }
+
+    function _getWindowSize() internal view virtual override(ERC20EXPBase, ERC20TLSW) returns (uint8) {
+        return super._getWindowSize();
+    }
+
+    function _getPointersInEpoch() internal view virtual override(ERC20EXPBase, ERC20TLSW) returns (uint40) {
+        return super._getPointersInEpoch();
+    }
+
+    function _getPointersInWindow() internal view virtual override(ERC20EXPBase, ERC20TLSW) returns (uint40) {
+        return super._getPointersInWindow();
+    }
+
+    function _pointerProvider() internal view virtual override(ERC20EXPBase, ERC20TLSW) returns (uint256) {
+        return super._pointerProvider();
+    }
+
+    function _update(address from, address to, uint256 value) internal virtual override(ERC20EXPBase, ERC7818Suspend) {
+        super._update(from, to, value);
+    }
+
+    function addToSuspend(address account) public {
+        _addToSuspend(account);
+    }
+
+    function removeFromSuspend(address account) public {
+        _removeFromSuspend(account);
+    }
+
+    function mint(address to, uint256 value) public {
+        _mint(to, value);
+    }
+
+    function burn(address to, uint256 value) public {
+        _burn(to, value);
+    }
+}
--- test/tokens/ERC20/extensions/index.test.ts
+++ test/tokens/ERC20/extensions/index.test.ts
@@ -3,6 +3,7 @@ import * as ERC7818Blacklist from "./ERC7818Blacklist/index.test";
 import * as ERC7818MintQuota from "./ERC7818MintQuota/index.test";
 import * as ERC7818Whitelist from "./ERC7818Whitelist/index.test";
 import * as ERC7818NearestExpiryQuery from "./ERC7818NearestExpiryQuery/index.test";
+import * as ERC7818Suspend from "./ERC7818Suspend/index.test";
 
 export const run = async ({epochType = constants.EPOCH_TYPE.BLOCKS_BASED}) => {
   describe("extensions", async function () {
@@ -10,5 +11,6 @@ export const run = async ({epochType = constants.EPOCH_TYPE.BLOCKS_BASED}) => {
     ERC7818MintQuota.run({epochType});
     ERC7818Whitelist.run({epochType});
     ERC7818NearestExpiryQuery.run({epochType});
+    ERC7818Suspend.run({epochType});
   });
 };
