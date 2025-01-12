8055b56614e7c1c93c07b96fbcf2f5d68603ddd5
ADISAKBOONMARK <adisak0661013@gmail.com>
2025-01-12 12:00:58 +0700

    build: add Suspend extension

---
+++ contracts/tokens/ERC20/extensions/ERC7818Suspend.sol
@@ -0,0 +1,81 @@
+// SPDX-License-Identifier: Apache-2.0
+pragma solidity >=0.8.0 <0.9.0;
+
+/// @title ERC20EXP Suspend extension
+/// @author Kiwari Labs
+
+import "../ERC20EXPBase.sol";
+
+abstract contract ERC7818Suspend is ERC20EXPBase {
+    /// @notice Emitted when an account is added to the suspended list.
+    /// @param caller The address that added the account to the suspended list.
+    /// @param account The address of the suspended account.
+    event AddedToSuspended(address indexed caller, address indexed account);
+
+    /// @notice Emitted when an account is removed from the suspended list.
+    /// @param caller The address that removed the account from the suspended list.
+    /// @param account The address of the reinstated account.
+    event RemovedFromSuspended(address indexed caller, address indexed account);
+
+    /// @notice Reverts if the account is suspended.
+    /// @param account The address of the suspended account.
+    error AccountSuspended(address account);
+
+    /// @notice Reverts if the account is not suspended.
+    /// @param account The address of the account that is not suspended.
+    error AccountNotSuspended(address account);
+
+    /// @dev Mapping to keep track of suspended accounts.
+    mapping(address => bool) private _suspended;
+
+    /// @notice Modifier to allow execution only if the account is suspended.
+    /// @param account The address to check.
+    modifier onlySuspended(address account) {
+        if (!isSuspended(account)) revert AccountNotSuspended(account);
+        _;
+    }
+
+    /// @notice Modifier to allow execution only if the account is not suspended.
+    /// @param account The address to check.
+    modifier onlyNotSuspended(address account) {
+        if (isSuspended(account)) revert AccountSuspended(account);
+        _;
+    }
+
+    /**
+     * @notice Checks if an account is currently suspended.
+     * @param account The address to check.
+     * @return True if the account is suspended, false otherwise.
+     */
+    function isSuspended(address account) public view returns (bool) {
+        return _suspended[account];
+    }
+
+    /**
+     * @notice Adds an account to the suspended list.
+     * @param account The address to suspend.
+     */
+    function _addToSuspended(address account) internal onlyNotSuspended(account) {
+        _suspended[account] = true;
+        emit AddedToSuspended(_msgSender(), account);
+    }
+
+    /**
+     * @notice Removes an account from the suspended list.
+     * @param account The address to reinstate.
+     */
+    function _removeFromSuspended(address account) internal onlySuspended(account) {
+        _suspended[account] = false;
+        emit RemovedFromSuspended(_msgSender(), account);
+    }
+
+    /**
+     * @notice Overrides the `_update` function to include suspension checks.
+     * @param from The address initiating the update.
+     * @param to The address receiving the update.
+     * @param value The value involved in the update.
+     */
+    function _update(address from, address to, uint256 value) internal virtual override onlyNotSuspended(from) {
+        super._update(from, to, value);
+    }
+}
