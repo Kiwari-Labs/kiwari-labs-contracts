3714e551379edc60cac032d6e84b6755784bd5cc
ADISAKBOONMARK <adisak0661013@gmail.com>
2025-01-16 13:33:04 +0700

    refactor: change naming of extension from Suspend to Frozen

--- test/tokens/ERC20/extensions/ERC7818Frozen/deployer.test.ts
+++ test/tokens/ERC20/extensions/ERC7818Frozen/deployer.test.ts
@@ -1,36 +1,36 @@
 import {ethers} from "hardhat";
-import {constants, ERC7818SuspendBLSW, ERC20, ERC7818SuspendTLSW} from "../../../../constant.test";
-import {MockERC7818SuspendBLSW} from "../../../../../typechain-types/mocks/contracts/tokens/ERC20/extensions/BLSW";
-import {MockERC7818SuspendTLSW} from "../../../../../typechain-types/mocks/contracts/tokens/ERC20/extensions/TLSW";
+import {constants, ERC7818FrozenBLSW, ERC20, ERC7818FrozenTLSW} from "../../../../constant.test";
+import {MockERC7818FrozenBLSW} from "../../../../../typechain-types/mocks/contracts/tokens/ERC20/extensions/BLSW";
+import {MockERC7818FrozenTLSW} from "../../../../../typechain-types/mocks/contracts/tokens/ERC20/extensions/TLSW";
 
-export const deployERC7818SuspendSelector = async function ({
+export const deployERC7818FrozenSelector = async function ({
   blocksPerEpoch = constants.DEFAULT_BLOCKS_PER_EPOCH, // 300 blocks
   secondsPerEpoch = constants.DEFAULT_SECONDS_PER_EPOCH, // 3_600 seconds.
   windowSize = constants.DEFAULT_WINDOW_SIZE, // fixed width window size 2 epoch.
   epochType = constants.EPOCH_TYPE.BLOCKS_BASED,
 } = {}) {
   if (epochType === constants.EPOCH_TYPE.BLOCKS_BASED) {
-    return await deployERC7818SuspendBLSW({blocksPerEpoch, windowSize});
+    return await deployERC7818FrozenBLSW({blocksPerEpoch, windowSize});
   }
 
-  return await deployERC7818SuspendTLSW({secondsPerEpoch, windowSize});
+  return await deployERC7818FrozenTLSW({secondsPerEpoch, windowSize});
 };
 
-export const deployERC7818SuspendTLSW = async function ({
+export const deployERC7818FrozenTLSW = async function ({
   secondsPerEpoch = constants.DEFAULT_SECONDS_PER_EPOCH, // 3_600 seconds.
   windowSize = constants.DEFAULT_WINDOW_SIZE, // fixed width window size 2 epoch.
 } = {}) {
   const [deployer, alice, bob, charlie] = await ethers.getSigners();
-  const ERC7818_SUSPEND = await ethers.getContractFactory(ERC7818SuspendTLSW.name, deployer);
-  const erc7818Suspend = (await ERC7818_SUSPEND.deploy(
+  const ERC7818_FROZEN = await ethers.getContractFactory(ERC7818FrozenTLSW.name, deployer);
+  const erc7818Frozen = (await ERC7818_FROZEN.deploy(
     ERC20.constructor.name,
     ERC20.constructor.symbol,
     secondsPerEpoch,
     windowSize,
-  )) as any as MockERC7818SuspendTLSW;
-  await erc7818Suspend.waitForDeployment();
+  )) as any as MockERC7818FrozenTLSW;
+  await erc7818Frozen.waitForDeployment();
   return {
-    erc7818Suspend,
+    erc7818Frozen,
     deployer,
     alice,
     bob,
@@ -38,21 +38,21 @@ export const deployERC7818SuspendTLSW = async function ({
   };
 };
 
-export const deployERC7818SuspendBLSW = async function ({
+export const deployERC7818FrozenBLSW = async function ({
   blocksPerEpoch = constants.DEFAULT_BLOCKS_PER_EPOCH, // 300 blocks
   windowSize = constants.DEFAULT_WINDOW_SIZE, // fixed width window size 2 epoch.
 } = {}) {
   const [deployer, alice, bob, charlie] = await ethers.getSigners();
-  const ERC7818_SUSPEND = await ethers.getContractFactory(ERC7818SuspendBLSW.name, deployer);
-  const erc7818Suspend = (await ERC7818_SUSPEND.deploy(
+  const ERC7818_FROZEN = await ethers.getContractFactory(ERC7818FrozenBLSW.name, deployer);
+  const erc7818Frozen = (await ERC7818_FROZEN.deploy(
     ERC20.constructor.name,
     ERC20.constructor.symbol,
     blocksPerEpoch,
     windowSize,
-  )) as any as MockERC7818SuspendBLSW;
-  await erc7818Suspend.waitForDeployment();
+  )) as any as MockERC7818FrozenBLSW;
+  await erc7818Frozen.waitForDeployment();
   return {
-    erc7818Suspend,
+    erc7818Frozen,
     deployer,
     alice,
     bob,
---
+++ test/tokens/ERC20/extensions/ERC7818Frozen/mint.test.ts
@@ -0,0 +1,29 @@
+import {expect} from "chai";
+import {deployERC7818FrozenSelector} from "./deployer.test";
+import {constants, ERC7818Frozen} from "../../../../constant.test";
+import {hardhat_reset} from "../../../../utils.test";
+
+export const run = async ({epochType = constants.EPOCH_TYPE.BLOCKS_BASED}) => {
+  describe("Mint", async function () {
+    const amount = 1;
+
+    afterEach(async function () {
+      await hardhat_reset();
+    });
+
+    it("[SUCCESS] mint `to` non-frozen", async function () {
+      const {erc7818Frozen, alice} = await deployERC7818FrozenSelector({epochType});
+      expect(await erc7818Frozen.isFrozen(alice.address)).to.equal(false);
+      await erc7818Frozen.mint(alice.address, amount);
+      expect(await erc7818Frozen.balanceOf(alice.address)).to.equal(amount);
+    });
+
+    it("[FAILED] mint `to` frozen", async function () {
+      const {erc7818Frozen, alice} = await deployERC7818FrozenSelector({epochType});
+      await erc7818Frozen.freeze(alice.address);
+      expect(await erc7818Frozen.isFrozen(alice.address)).to.equal(true);
+      await erc7818Frozen.mint(alice.address, amount);
+      expect(await erc7818Frozen.balanceOf(alice.address)).to.equal(amount);
+    });
+  });
+};
